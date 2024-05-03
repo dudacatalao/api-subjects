b258bd9cea216e11121a14667bd4941598f11eb9
Maria Eduarda <dmelocatalao@gmail.com>
2024-05-02 23:00:50 -0300

    making front

---
+++ front-subjects/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './home.css'
+
+
+const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  return (
+    <div>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
