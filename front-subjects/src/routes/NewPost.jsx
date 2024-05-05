b0c2f18588ae2fff14b09478b0552c0296d50ca3
Maria Eduarda <dmelocatalao@gmail.com>
2024-05-05 10:34:16 -0300

    ok

--- front-subjects/src/routes/NewPost.jsx
+++ front-subjects/src/routes/NewPost.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import './newpost.css'
+import apiFetch from '../axios/config'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async (e) =>{
+    e.preventDefault()
+
+    const post = [title, body];
+
+    await apiFetch.post('/posts', {
+      body:post
+    })
+
+    navigate('/')
+  }
+
   return (
-    <div>
-      <div></div>
+    <div className='new-post'>
+      <h2>Inserir new post:</h2>
+      <form action="" onSubmit={(e) => createPost(e)}>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input 
+            type="text" 
+            name="title" 
+            id="title"
+            placeholder='Digite o título' 
+            onChange={() => setTitle(e.target.value)}/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea 
+            name='body'
+            id='body' 
+            placeholder='Digite o conteúdo'
+            onChange={() => setBody(e.target.value)}
+            ></textarea>
+        </div>
+        <input type="submit" value='Criar Post' className='btn' />
+      </form>
     </div>
   )
 }
