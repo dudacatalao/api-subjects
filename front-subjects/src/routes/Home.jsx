221268703339eeadd169c042a9940bf1de1ff15e
Maria Eduarda <dmelocatalao@gmail.com>
2024-05-05 02:31:11 -0300

    front ok

--- front-subjects/src/routes/Home.jsx
+++ front-subjects/src/routes/Home.jsx
@@ -1,14 +1,39 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import './home.css'
-
+import apiFetch from "../axios/config"
 
 const Home = () => {
   const [posts, setPosts] = useState([])
 
+  const getPosts = async() =>{
+    try {
+      const response = await apiFetch.get("/posts")
+
+      const data = response.data
+      setPosts(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="home">
+        <h1>Ultimos Posts</h1>
+        {posts.length === 0 ? (<p>Carregando</p>) : (
+          posts.map((post) => (
+            <div className="post" key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+            </div>
+          ))
+        )}
     </div>
   )
 }
--- front-subjects/src/routes/NewPost.jsx
+++ front-subjects/src/routes/NewPost.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NewPost = () => {
   return (
     <div>
-      <h1>new post</h1>
+      <div></div>
     </div>
   )
 }
